f8994b1f84cd186322121ad1d1474d769aadafdb
Laptop Nano <Laptop Nano@LaptopNano-PC>
2020-07-23 22:37:45 +0700

    First commit

---
+++ karros-track-profiler-api-application/src/main/java/com/karros/application/KarrosTrackProfilerApiApplication.java
@@ -0,0 +1,13 @@
+package com.karros.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarrosTrackProfilerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarrosTrackProfilerApiApplication.class, args);
+	}
+
+}
---
+++ karros-track-profiler-api-application/src/test/java/com/karros/application/KarrosTrackProfilerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karros.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarrosTrackProfilerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
