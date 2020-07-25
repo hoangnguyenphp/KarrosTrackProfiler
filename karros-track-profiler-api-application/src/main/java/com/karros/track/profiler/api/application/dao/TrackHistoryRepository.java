c77aa40ee33176bb513c44717ef2c360cda427cf
Laptop Nano <Laptop Nano@LaptopNano-PC>
2020-07-25 22:47:26 +0700

    commit

--- karros-track-profiler-api-application/src/main/java/com/karros/track/profiler/api/application/dao/TrackHistoryRepository.java
+++ karros-track-profiler-api-application/src/main/java/com/karros/track/profiler/api/application/dao/TrackHistoryRepository.java
@@ -1,8 +1,5 @@
 package com.karros.track.profiler.api.application.dao;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.karros.track.profiler.api.application.model.TrackHistory;
